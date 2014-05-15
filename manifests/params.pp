dfcff8b8f6aa9227434fc1075746e94fd4b59c50
Pascal Immerzeel <pascal@immerzeelpictures.com>
2014-05-15 23:33:04 +0200

    Simplify VM settings

--- manifests/params.pp
+++ manifests/params.pp
@@ -3,16 +3,9 @@ class dotfiles::params {
 	$source = 'git://git@github.com/immerzeel/dotfiles.git'
 	$user   = ''
 
-	case $::osfamily {
-		'Ubuntu': {
-			# On virtual machine with Ubuntu, default to 'vagrant'.
-			if $::is_virtual {
-				$user   = 'vagrant'
-				$path   = '/home/vagrant'
-			}
-		}
-		default: {
-			fail("The ${module_name} module is not supported on an ${::osfamily} based system.")
-		}
+	# On virtual machine, default to 'vagrant'.
+	if $::is_virtual {
+		$user   = 'vagrant'
+		$path   = '/home/vagrant'
 	}
 }
